96b799cfe4aae69db24a354d6c0b5b60e56e0e6f
viktoria <viiikiii.g@gmail.com>
2023-07-08 20:27:46 +0200

    add javadoc

--- src/main/java/com/example/pizza/service/CafeService.java
+++ src/main/java/com/example/pizza/service/CafeService.java
@@ -1,19 +1,49 @@
 package com.example.pizza.service;
 
 import com.example.pizza.entity.Cafe;
-import com.example.pizza.entity.Customer;
-import com.example.pizza.entity.Pizza;
 
 import java.util.List;
-import java.util.UUID;
 
+/**
+ * The CafeService interface provides methods for managing cafes.
+ */
 public interface CafeService {
-     void createNewCafe(Cafe cafe);
 
+    /**
+     * Creates a new cafe.
+     *
+     * @param cafe the cafe to create
+     */
+    void createNewCafe(Cafe cafe);
+
+    /**
+     * Returns a list of all cafes.
+     *
+     * @return a list of all cafes
+     */
     List<Cafe> findAll();
-    Cafe findById(UUID uuid);
 
-    Cafe update(UUID uuid, Cafe cafe);
+    /**
+     * Finds a cafe by its ID.
+     *
+     * @param id the ID of the cafe to find
+     * @return the cafe with the specified ID, or null if not found
+     */
+    Cafe findById(Integer id);
 
-    void delete(UUID uuid);
+    /**
+     * Updates a cafe with the specified ID.
+     *
+     * @param id    the ID of the cafe to update
+     * @param cafe  the updated cafe object
+     * @return the updated cafe
+     */
+    Cafe update(Integer id, Cafe cafe);
+
+    /**
+     * Deletes a cafe with the specified ID.
+     *
+     * @param id the ID of the cafe to delete
+     */
+    void delete(Integer id);
 }
--- src/main/java/com/example/pizza/service/CustomerService.java
+++ src/main/java/com/example/pizza/service/CustomerService.java
@@ -3,16 +3,47 @@ package com.example.pizza.service;
 import com.example.pizza.entity.Customer;
 
 import java.util.List;
-import java.util.UUID;
 
+/**
+ * The CustomerService interface provides methods for managing customers.
+ */
 public interface CustomerService {
+
+    /**
+     * Creates a new customer.
+     *
+     * @param customer the customer to create
+     */
     void createNewCustomer(Customer customer);
 
+    /**
+     * Returns a list of all customers.
+     *
+     * @return a list of all customers
+     */
     List<Customer> findAll();
 
+    /**
+     * Finds a customer by their ID.
+     *
+     * @param id the ID of the customer to find
+     * @return the customer with the specified ID, or null if not found
+     */
     Customer findById(Integer id);
 
+    /**
+     * Updates a customer with the specified ID.
+     *
+     * @param id       the ID of the customer to update
+     * @param customer the updated customer object
+     * @return the updated customer
+     */
     Customer update(Integer id, Customer customer);
 
+    /**
+     * Deletes a customer with the specified ID.
+     *
+     * @param id the ID of the customer to delete
+     */
     void delete(Integer id);
 }
