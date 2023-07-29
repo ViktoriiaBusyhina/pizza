8d0f1e95e299c6e43b570fa4ec0aabd0723e0d4a
viktoria <viiikiii.g@gmail.com>
2023-07-29 14:52:40 +0200

    Add new entity

---
+++ src/main/java/com/example/pizza/entity/UserAuthority.java
@@ -0,0 +1,18 @@
+package com.example.pizza.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "authorities")
+public class UserAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    @ManyToOne
+    @JoinColumn(name = "username", referencedColumnName = "username")
+    private User user;
+}
